68d8f486836d0333472e9c19a0c704b8f4e3b613
leesoeul <thdmfrhdwn@naver.com>
2024-07-23 14:26:59 +0900

    software request mybatis
    폴더 resources/mappers -> resources/mapper

--- src/main/java/com/prix/homepage/download/service/RequestService.java
+++ src/main/java/com/prix/homepage/download/service/RequestService.java
@@ -1,7 +1,12 @@
 package com.prix.homepage.download.service;
 
+import com.prix.homepage.download.pojo.SoftwareRequest;
+
 public interface RequestService {
 
-    void insert(String name, String affiliation, String title, String email, String instrument,
-            String software);
+    void insert(String name, String affiliation, String title, String email,
+            String instrument,
+            String software) throws Exception;
+
+    void insertRequest(SoftwareRequest request) throws Exception;
 }
--- src/main/java/com/prix/homepage/download/service/impl/RequestServiceImpl.java
+++ src/main/java/com/prix/homepage/download/service/impl/RequestServiceImpl.java
@@ -6,7 +6,6 @@ import com.prix.homepage.download.service.RequestService;
 
 import lombok.AllArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -15,44 +14,29 @@ import java.sql.Timestamp;
 @Service
 public class RequestServiceImpl implements RequestService {
 
-        private final RequestMapper requestMapper;
+  private final RequestMapper requestMapper;
 
-        @Override
-        public void insert(String name, String affiliation, String title, String email, String instrument,
-                        String software) {
+  @Override
+  public void insertRequest(SoftwareRequest request) {
+    requestMapper.saveRequest(request);
+  }
 
-                SoftwareRequest request = SoftwareRequest.builder()
-                                .date(new Timestamp(System.currentTimeMillis()))
-                                .name(name)
-                                .affiliation(affiliation)
-                                .title(title)
-                                .email(email)
-                                .instrument(instrument)
-                                .software(software)
-                                .version("1.0") // Assuming a default version
-                                .state(0)
-                                .date(new Timestamp(System.currentTimeMillis()))
-                                .build();
+  @Override
+  public void insert(String name, String affiliation, String title, String email, String instrument,
+      String software) {
 
-                // SoftwareRequest request = new SoftwareRequest();
-                // request.setDate(new Timestamp(System.currentTimeMillis()));
-                // request.setName(name);
-                // request.setAffiliation(affiliation);
-                // request.setTitle(title);
-                // request.setEmail(email);
-                // request.setInstrument(instrument);
-                // request.setSoftware(software);
-                // request.setState(
-
-                // request.setSenttime(null);
-                // requestRepository.save(request);
-
-                requestMapper.saveRequest(request);
-                // requestMapper.saveRequest(new Timestamp(System.currentTimeMillis()), name,
-                // affiliation, title, email, instrument,
-                // software, "1.0", 0, null);
-                // System.out.println("Inserted request ID: " + request.getId());
-
-        }
+    requestMapper.saveRequest(SoftwareRequest.builder()
+        .date(new Timestamp(System.currentTimeMillis()))
+        .name(name)
+        .affiliation(affiliation)
+        .title(title)
+        .email(email)
+        .instrument(instrument)
+        .software(software)
+        .version("1.0") // Assuming a default version
+        .state(0)
+        .date(new Timestamp(System.currentTimeMillis()))
+        .build());
+  }
 
 }
