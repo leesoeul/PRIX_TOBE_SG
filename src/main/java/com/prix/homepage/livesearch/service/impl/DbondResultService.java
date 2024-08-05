29f39cc6afe7a582d5512185c5d2d04bfd0fa46a
kimye0808 <kyh011@hanyang.ac.kr>
2024-08-05 20:51:47 +0900

    fix: dbond result 페이지 extract as 기능 및  DownloadServlet 고침

--- src/main/java/com/prix/homepage/livesearch/service/impl/DbondResultService.java
+++ src/main/java/com/prix/homepage/livesearch/service/impl/DbondResultService.java
@@ -1,12 +1,9 @@
 package com.prix.homepage.livesearch.service.impl;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Comparator;
 
