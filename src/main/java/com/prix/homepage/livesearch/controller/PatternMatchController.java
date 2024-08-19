615877c774bd4c3ae110d5f68622f5970511a40d
leesoeul <thdmfrhdwn@naver.com>
2024-08-19 11:48:59 +0900

    pattern match result rendering
    pattern match 결과값을 patternMatchResult.html 페이지에 랜더링.
    기존 out.print 방식과 다르게 클라이언트측에 결과값을 보낸 후 랜더링.
    결과 데이터셋의 크기가 큰 경우 랜더링 시간 초과로 터지는 버그 발생.
    -> 페이지네이션으로 해결

--- src/main/java/com/prix/homepage/livesearch/controller/PatternMatchController.java
+++ src/main/java/com/prix/homepage/livesearch/controller/PatternMatchController.java
@@ -1,6 +1,7 @@
 package com.prix.homepage.livesearch.controller;
 
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.prix.homepage.livesearch.patternMatch.PatternMatch;
 import com.prix.homepage.livesearch.pojo.PatternMatchDto;
 import com.prix.homepage.livesearch.service.PatternMatchService;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @AllArgsConstructor
@@ -60,8 +59,6 @@ public class PatternMatchController {
             String db_t = patternMatchDto.getDb_type().equals("0") ? "swiss_prot" : "genbank";
             String sd = "";
             sd = patternMatchService.getUpdateDay(db_t);
-            // out.print("<li>" + db_t + " Release : (" + sd + ")</li><br>");
-            // out.flush(); // 클라이언트에게 즉시 전송
 
             String[] pattern = { patternMatchDto.getPattern1(),
                     patternMatchDto.getPattern2(),
@@ -80,35 +77,33 @@ public class PatternMatchController {
             PatternMatch p = new PatternMatch(format_type, db_t, pattern, check_species,
                     species, check_except, check_order);
             p.DBParameter("prix", "Prix2024!@", "166.104.110.37/");
-            // System.out.println("Form type: " + format_type + ", db_type: " + db_t);
-
-            StringBuilder result = new StringBuilder();
             p.MainMethod();
-            // String oneResult = p.getOneProtein();
+
+            List<String> allResults = new ArrayList<String>();
             String oneResult = "";
-            // p.MainMethod();
             while (oneResult != null) {
-                result.append(oneResult);
-                // out.print(oneResult + "<br>"); // 결과를 화면에 출력
-                oneResult = p.getOneProtein();
-                // out.flush(); // 클라이언트에게 바로 전송
+                allResults.add(oneResult);
+                oneResult = p.getOneProtein(); // 검색된 결과를 하나씩 가져온다
             }
-            result.append(p.getParameter());
-            // out.println(p.getParameter() + "<br>");
+
+            allResults.add(p.getParameter()); // 마지막에 찾은 protein과 pattern의 수를 화면에 출력
 
             model.addAttribute("dbType", db_t);
             model.addAttribute("date", sd);
-            model.addAttribute("result", result.toString());
+            model.addAttribute("allResults", allResults); // 조건에 맞는 데이터set을 넘겨줌
 
         } catch (Exception e) {
-            // try (PrintWriter out = response.getWriter()) {
-            // out.println("Error");
-            // out.println("" + e.getStackTrace()[0]);
-            // e.printStackTrace(out);
-            // } catch (Exception ex) {
             e.printStackTrace();
-            // }
         }
+        // 08.18 메모
+        // db에서 데이터를 실시간으로 받아오는 것이 아니었다.. 데이터가 클수록 가져오는데 시간이 오래걸림.
+        // 실시간으로 받아오는 것처럼 보이는 건 랜더링 로딩이었다!
+        // controller에서 out.print가 아닌 result.html 페이지에서 데이터를 랜더링하면 데이터가 큰 경우 터지는 버그ㅜ
+        // 페이지 네이션은 실패
+        // html 파일을 랜더링 하지 않고 파비콘과 탭 title을 설정하는 방법 구상해보기 끼야아아악
+        // 08.19 메모
+        // 한 번 가져온 result를 활용하자! db에서 받아온 데이터를 모두 클라이언트에게 넘긴다.
+        // -> 클라이언트에서 페이지네이션 진행. 넘겨받은 result 중 일부만 랜더링하는 방식.
 
         return "livesearch/patternMatchResult";
     }
